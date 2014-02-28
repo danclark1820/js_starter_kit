5bd67f403cad2d8e9ad776742d47d3c5a4246ff3
danclark1820 <danclark1820@gmail.com>
2014-02-28 15:22:13 -0500

    First JavaScript challenge

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-alert('Hey find me to know where to put the javascript');
+var random = Math.floor((Math.random()*10) + 1);
+var user_name = prompt('What is your name?');
+var guess = prompt('Hey ' + user_name +', Guess a number between 1 and 10');
+
+if (guess == random){
+  alert("You Win!");
+} else {
+  alert("You Lose");
+}
