6ca684ed7a08f2ae7a5ea708a9b469538215d0de
danclark1820 <danclark1820@gmail.com>
2014-02-28 19:48:22 -0500

    Started guess the number 2

--- js/app.js
+++ js/app.js
@@ -1,9 +1,22 @@
-var random = Math.floor((Math.random()*10) + 1);
-var user_name = prompt('What is your name?');
-var guess = prompt('Hey ' + user_name +', Guess a number between 1 and 10');
+var max = 10;
+var random = Math.floor((Math.random()*max) + 1);
+var userName = prompt('What is your name?');
+var guess = prompt('Hey ' + userName +', Guess a number between 1 and ' + max);
+var count = 0;
+var play = "yes";
 
-if (guess == random){
-  alert("You Win!");
-} else {
-  alert("You Lose");
-}
+// wrap in a method and use and call the method again if the player wins
+    while (guess != random){
+        if (guess > random) {
+            guess = prompt(guess + " was too high, try again...");
+            count++;
+        } else {
+            guess = prompt(guess + " was too low, try again...");
+            count++;
+        }
+    }
+
+    if (guess == random) {
+        count++;
+        alert("Congratulations, you've guessed the number in " + count + " guesses!");
+    }
